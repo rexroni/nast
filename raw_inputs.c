bbe5ba32ae2093601e4ecd249b073c6efd8861e7
Rex Roni <rexroni7-general@yahoo.com>
2021-08-22 13:46:02 -0600

    more raw_inputs

--- raw_inputs.c
+++ raw_inputs.c
@@ -33,7 +33,9 @@ void show_characters_from_tty(int appcursor, int appkeypad){
     char byte;
     while(read(0, &byte, 1) == 1){
         if(byte == 'q') break;
-        printf("dec = %d, hex = %x, char = %c\r\n", (int)byte, (int)byte, byte);
+        int b = byte;
+        if(b < 0) b += 256;
+        printf("dec = %d, hex = %x, char = %c\r\n", (int)b, (int)b, b);
     }
 
     if(appcursor){
@@ -72,9 +74,10 @@ int wait_for_input(int fd, suseconds_t usec, int *ok){
 }
 
 // reads a string of input, returning after a short interval without input
-int read_string(char *buf, size_t len){
+int read_string(char *buf, size_t len, size_t *nread_out){
     char byte;
     size_t nread = 0;
+    *nread_out = nread;
     while(nread < len - 1){
         if(nread > 0){
             int ok;
@@ -93,6 +96,7 @@ int read_string(char *buf, size_t len){
         buf[nread++] = byte;
     }
     buf[nread] = '\0';
+    *nread_out = nread;
     return 0;
 }
 
@@ -119,13 +123,13 @@ char hex(int v){
 }
 
 // log chars as you would type them into a string literal
-int fstrlit(FILE *f, char *s){
+int fstrlit(FILE *f, char *s, size_t slen){
 #   define CHR(x) buf[len++] = x
     char buf[128];
     size_t len = 0, maxlen = sizeof(buf);
     char c;
     size_t i;
-    for(i = 0, c = s[i]; len < maxlen && c != '\0'; c = s[++i]){
+    for(i = 0, c = s[i]; len < maxlen && i < slen; c = s[++i]){
         if     (c == '\0'){ CHR('\\'); CHR('0'); }
         else if(c == '\r'){ CHR('\\'); CHR('r'); }
         else if(c == '\n'){ CHR('\\'); CHR('n'); }
@@ -181,33 +185,37 @@ int run_logger(char *logfile){
         tcsetattr(0, TCSANOW, &raw_tios);
 
         char norm[16];
+        size_t norm_len;
         char cur[16];
+        size_t cur_len;
         char key[16];
+        size_t key_len;
         char curkey[16];
+        size_t curkey_len;
 
         printf("press it in normal mode\r\n");
-        if(read_string(norm, sizeof(norm))){
+        if(read_string(norm, sizeof(norm), &norm_len)){
             retval = 1;
             goto cu;
         }
 
         printf(APPCURSOR_ON "press it with appcursor mode\r\n");
         fflush(stdout);
-        if(read_string(cur, sizeof(cur))){
+        if(read_string(cur, sizeof(cur), &cur_len)){
             retval = 1;
             goto cu;
         }
 
         printf(APPCURSOR_OFF APPKEYPAD_ON "press it with appkeypad mode\r\n");
         fflush(stdout);
-        if(read_string(key, sizeof(key))){
+        if(read_string(key, sizeof(key), &key_len)){
             retval = 1;
             goto cu;
         }
 
         printf(APPCURSOR_ON "press it with both modes\r\n");
         fflush(stdout);
-        if(read_string(curkey, sizeof(curkey))){
+        if(read_string(curkey, sizeof(curkey), &curkey_len)){
             retval = 1;
             goto cu;
         }
@@ -216,10 +224,13 @@ int run_logger(char *logfile){
         fflush(stdout);
 
         fprintf(f, "%s,", name);
-        fstrlit(f, norm); fprintf(f, ",");
-        fstrlit(f, cur); fprintf(f, ",");
-        fstrlit(f, key); fprintf(f, ",");
-        fstrlit(f, curkey); fprintf(f, "\n");
+        fstrlit(f, norm, norm_len); fprintf(f, ",");
+        fstrlit(f, cur, cur_len); fprintf(f, ",");
+        fstrlit(f, key, key_len); fprintf(f, ",");
+        fstrlit(f, curkey, curkey_len); fprintf(f, "\n");
+
+        fflush(f);
+        fsync(fileno(f));
     }
 
 
